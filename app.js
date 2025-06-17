1aed5cece4d75a01982ee2b1538c3390637f2df9
VCRGPT <veteranscommunityresource@gmail.com>
2025-06-17 10:31:13 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// For regular fields: add class 'field-group'
-function createFieldGroup(title, content, italic = false, spaced3 = false) {
-  const container = document.createElement('p');
-  container.classList.add('field-group');
-  if (spaced3) container.classList.add('spaced-3');
-
-  // ... rest remains unchanged
-}
